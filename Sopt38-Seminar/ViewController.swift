1df3f704804dd3a620d81704fd97d2ea43785b27
jiinjang <jiinjang0@gmail.com>
2026-04-10 02:27:13 +0900

    first commit

--- Sopt38-Seminar/ViewController.swift
+++ Sopt38-Seminar/ViewController.swift
@@ -5,15 +5,35 @@
 //  Created by 장지인 on 4/10/26.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
     }
-
-
 }
 
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getFontName()
+    }
+    
+    func getFontName() {
+        for family in UIFont.familyNames {
+
+            let sName: String = family as String
+            print("family: \(sName)")
+                    
+            for name in UIFont.fontNames(forFamilyName: sName) {
+                print("name: \(name as String)")
+            }
+        }
+    }
+}
