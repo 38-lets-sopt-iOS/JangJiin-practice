c4d688eaea11bee86a3e493234ca45519b6765f3
jiinjang <jiinjang0@gmail.com>
2026-04-10 22:13:56 +0900

    [setting] 프로젝트 초기세팅

---
+++ Sopt-38-practice/Sopt-38-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt-38-practice
+//
+//  Created by 장지인 on 4/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
