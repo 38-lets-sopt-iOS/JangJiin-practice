2cee0478d4d8036699389cf4071afd8ae9d1739c
jiinjang <jiinjang0@gmail.com>
2026-04-30 14:29:42 +0900

    [feat] 3주차 실습

--- Sopt-38-practice/Sopt-38-practice/SceneDelegate.swift
+++ Sopt-38-practice/Sopt-38-practice/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let loginViewController = AutoLayout_Practice()
-        let navigationController = UINavigationController(rootViewController: loginViewController)
+        let rootViewController = ItemViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
         
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = navigationController
