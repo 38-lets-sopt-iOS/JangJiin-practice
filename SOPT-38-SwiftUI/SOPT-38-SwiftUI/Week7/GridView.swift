a8d09755b879b1bb8a6a718b8fa594ef482896dd
jiinjang <jiinjang0@gmail.com>
2026-05-29 17:55:59 +0900

    [feat] 7주차 실습

---
+++ SOPT-38-SwiftUI/SOPT-38-SwiftUI/Week7/GridView.swift
@@ -0,0 +1,36 @@
+//
+//  Grid.swift
+//  
+//
+//  Created by 장지인 on 5/23/26.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Grid {
+            GridRow {
+                Text("Row 1")
+                ForEach(0..<2) { _ in Color.red }
+            }
+            .frame(width: 50, height: 50)
+            
+            GridRow {
+                Text("Row 2")
+                ForEach(0..<5) { _ in Color.green }
+            }
+            .frame(width: 50, height: 50)
+            
+            GridRow {
+                Text("Row 3")
+                ForEach(0..<4) { _ in Color.blue }
+            }
+            .frame(width: 50, height: 50)
+        }
+    }
+}
+
+#Preview {
+    GridView()
+}
